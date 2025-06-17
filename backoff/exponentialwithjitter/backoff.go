911ed45604900e715c47f35d82bf72cb934b149a
tandem97 <tandemic97@gmail.com>
2025-06-17 10:32:35 +0300

    improve exponential backoff with jitter

--- backoff/exponentialwithjitter/backoff.go
+++ backoff/exponentialwithjitter/backoff.go
@@ -1,23 +1,26 @@
 package exponentialwithjitter
 
 import (
+	"math"
 	"math/rand"
 	"sync"
 	"time"
 )
 
 const (
-	DefaultBase             = time.Second
-	DefaultCap              = 30 * time.Second
-	DefaultJitterMultiplier = 2
+	DefaultBase       = 1 * time.Second
+	DefaultCap        = 30 * time.Second
+	DefaultMultiplier = 1.6
+	DefaultJitter     = 0.2
 )
 
 type Backoff struct {
-	base             time.Duration
-	cap              time.Duration
-	jitterMultiplier int64
-	backoff          time.Duration
-	mu               sync.Mutex
+	base       time.Duration
+	cap        time.Duration
+	jitter     float64
+	multiplier float64
+	retries    int
+	mu         sync.Mutex
 }
 
 type opt func(b *Backoff)
@@ -34,27 +37,28 @@ func WithCap(cap time.Duration) opt {
 	}
 }
 
-func WithJitterMultiplier(multiplier int64) opt {
+func WithMultiplier(multiplier float64) opt {
 	return func(b *Backoff) {
-		b.jitterMultiplier = multiplier
+		b.multiplier = multiplier
+	}
+}
+
+func WithJitter(jitter float64) opt {
+	return func(b *Backoff) {
+		b.jitter = jitter
 	}
 }
 
 func New(opts ...opt) *Backoff {
 	backoff := &Backoff{
-		base:             DefaultBase,
-		cap:              DefaultCap,
-		jitterMultiplier: DefaultJitterMultiplier,
+		base:       DefaultBase,
+		cap:        DefaultCap,
+		multiplier: DefaultMultiplier,
+		jitter:     DefaultJitter,
 	}
 
 	for _, opt := range opts {
 		opt(backoff)
-	}
-
-	backoff.backoff = backoff.base
-
-	if backoff.jitterMultiplier == 0 {
-		backoff.jitterMultiplier = 1
 	}
 
 	return backoff
@@ -64,19 +68,37 @@ func (b *Backoff) Backoff() time.Duration {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 
-	if b.backoff > b.cap {
-		b.backoff = b.cap
+	r := b.retries
+
+	if b.retries != math.MaxInt {
+		b.retries++
 	}
 
-	jitter := rand.Int63n(int64(b.backoff) * b.jitterMultiplier)
-	b.backoff <<= 1
+	if r == 0 {
+		return b.base
+	}
 
-	return b.base + time.Duration(jitter)
+	backoff, max := float64(b.base), float64(b.cap)
+	for backoff < max && r > 0 {
+		backoff *= b.multiplier
+		r--
+	}
+
+	if backoff > max {
+		backoff = max
+	}
+
+	backoff *= 1 + b.jitter*(rand.Float64()*2-1)
+	if backoff < 0 {
+		return 0
+	}
+
+	return time.Duration(backoff)
 }
 
 func (b *Backoff) Reset() {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 
-	b.backoff = b.base
+	b.retries = 0
 }
